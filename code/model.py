e053d719095b9ed128465da0d6af744c9bc64649
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2018-03-03 23:22:16 +0000

    Retrain augmentation model

--- code/model.py
+++ code/model.py
@@ -25,6 +25,7 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
@@ -33,7 +34,14 @@ model.compile(loss='binary_crossentropy',
               metrics=['acc'])
 
 # All images will be rescaled by 1./255
-train_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
@@ -41,19 +49,19 @@ train_generator = train_datagen.flow_from_directory(
         conf.train_dir,
         # All images will be resized to 150x150
         target_size=(150, 150),
-        batch_size=20,
+        batch_size=32,
         # Since we use binary_crossentropy loss, we need binary labels
         class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
         conf.validation_dir,
         target_size=(150, 150),
-        batch_size=20,
+        batch_size=32,
         class_mode='binary')
 
 hist = model.fit_generator(
       train_generator,
-      steps_per_epoch=100,
+      steps_per_epoch=50,
       epochs=30,
       validation_data=validation_generator,
       validation_steps=50)
