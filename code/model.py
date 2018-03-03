ecea88bbd46c955cd42821d6cab593444b874a4f
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2018-03-03 23:09:18 +0000

    First trained model

--- code/model.py
+++ code/model.py
@@ -1,51 +1,39 @@
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
 
-import os
 from keras import layers
 from keras import models
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
-from keras.applications import VGG16
 import pickle
 
 import conf
 
-conv_base = VGG16(weights='imagenet',
-                  include_top=False,
-                  input_shape=(150, 150, 3))
-
 model = models.Sequential()
-model.add(conv_base)
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
-model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
-conv_base.trainable = True
-set_trainable = False
-for layer in conv_base.layers:
-    if layer.name == 'block5_conv1':
-        set_trainable = True
-    if set_trainable:
-        layer.trainable = True
-    else:
-        layer.trainable = False
-
 model.compile(loss='binary_crossentropy',
-              optimizer=optimizers.RMSprop(lr=1e-5),
+              optimizer=optimizers.RMSprop(lr=1e-4),
               metrics=['acc'])
 
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=40,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest')
-
-# Note that the validation data should not be augmented!
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
@@ -66,7 +54,7 @@ validation_generator = test_datagen.flow_from_directory(
 hist = model.fit_generator(
       train_generator,
       steps_per_epoch=100,
-      epochs=100,
+      epochs=30,
       validation_data=validation_generator,
       validation_steps=50)
 
