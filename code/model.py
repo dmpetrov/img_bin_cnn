a9e0c9cb91ed381289fdd11fc86b7e86b7e53d8a
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2018-03-04 00:27:03 +0000

    VGG16 with augm

--- code/model.py
+++ code/model.py
@@ -10,30 +10,27 @@ from keras import layers
 from keras import models
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import VGG16
 import pickle
 
 import conf
 
+conv_base = VGG16(weights='imagenet',
+                  include_top=False,
+                  input_shape=(150, 150, 3))
+
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu',
-                        input_shape=(150, 150, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
+model.add(conv_base)
 model.add(layers.Flatten())
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(256, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
+conv_base.trainable = False
+
 model.compile(loss='binary_crossentropy',
-              optimizer=optimizers.RMSprop(lr=1e-4),
+              optimizer=optimizers.RMSprop(lr=2e-5),
               metrics=['acc'])
 
-# All images will be rescaled by 1./255
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     rotation_range=40,
@@ -41,7 +38,10 @@ train_datagen = ImageDataGenerator(
     height_shift_range=0.2,
     shear_range=0.2,
     zoom_range=0.2,
-    horizontal_flip=True,)
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+# Note that the validation data should not be augmented!
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
@@ -49,22 +49,23 @@ train_generator = train_datagen.flow_from_directory(
         conf.train_dir,
         # All images will be resized to 150x150
         target_size=(150, 150),
-        batch_size=32,
+        batch_size=20,
         # Since we use binary_crossentropy loss, we need binary labels
         class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
         conf.validation_dir,
         target_size=(150, 150),
-        batch_size=32,
+        batch_size=20,
         class_mode='binary')
 
 hist = model.fit_generator(
       train_generator,
-      steps_per_epoch=50,
+      steps_per_epoch=100,
       epochs=30,
       validation_data=validation_generator,
-      validation_steps=50)
+      validation_steps=50,
+      verbose=2)
 
 model.save(os.path.join(conf.data_dir, 'model.h5'))
 pickle.dump(hist.history, open(os.path.join(conf.data_dir, 'history.p'), 'wb'))
